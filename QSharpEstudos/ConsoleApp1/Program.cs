00c93d23d2e31b40c4410de24feae3b38b5793e0
Flakes <ed.afram@gmail.com>
2020-06-13 16:04:26 -0300

    Inclusão de console application junto com operações quantum.

---
+++ QSharpEstudos/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Quantum.Simulation.Simulators;
+using System;
+using CustomAlgorithms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var sim = new QuantumSimulator();
+            
+            var num = PureRandomNumber.Run(sim).Result;
+            
+            Console.WriteLine(num);
+        }
+    }
+}
